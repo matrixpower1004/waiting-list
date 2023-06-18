49cd11ea64dc93f997c68cec1615a590365e008c
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-19 08:39:16 +0900

    Add #3 - api controllers
    API 문서에 정의된 view api controller 작성

---
+++ src/main/java/me/matrix/waitinglist/controller/api/APIEventController.java
@@ -0,0 +1,41 @@
+package me.matrix.waitinglist.controller.api;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * author         : Jason Lee
+ * date           : 2023-06-18
+ * description    :
+ */
+@RequestMapping("/api)")
+@RestController
+public class APIEventController {
+
+    @GetMapping("/events")
+    public List<String> getEvents() {
+        return List.of("event1", "event2");
+    }
+
+    @PostMapping("/events")
+    public Boolean createEvent() {
+        return true;
+    }
+
+    @GetMapping("/events/{eventId}")
+    public String getEvent(@PathVariable Integer eventId) {
+        return "event " + eventId;
+    }
+
+    @PutMapping("/events/{eventId}")
+    public Boolean modifyEvent(@PathVariable Integer eventId) {
+        return true;
+    }
+
+    @DeleteMapping("/events/{eventId}")
+    public Boolean removeEvent(@PathVariable Integer eventId) {
+        return true;
+    }
+
+}
