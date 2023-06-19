c51977817c3502311accaf1b3d74826bd3449941
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-19 18:13:31 +0900

    Update #3 - admin view controllers and views
    * 어드민 뷰 메소드에 인자 입력 및 모델 전달 코드 추가.
    * 관찰 가능하도록 간단히 일부 뷰에 타임리프 템플릿 코드 추가.

--- src/main/java/me/matrix/waitinglist/controller/AdminController.java
+++ src/main/java/me/matrix/waitinglist/controller/AdminController.java
@@ -1,9 +1,16 @@
 package me.matrix.waitinglist.controller;
 
+import me.matrix.waitinglist.constant.PlaceType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * author         : Jason Lee
@@ -15,8 +22,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping("/places")
-    public String adminPlaces() {
-        return "admin/places";
+    public ModelAndView adminPlaces(PlaceType placeType,
+                              String placeName,
+                              String address)
+    {
+        Map<String, Object> map = new HashMap<>();
+        map.put("placeType", placeType);
+        map.put("placeName", placeName);
+        map.put("address", address);
+
+        return new ModelAndView("admin/places", map);
     }
 
     @GetMapping("/places/{placeId}")
